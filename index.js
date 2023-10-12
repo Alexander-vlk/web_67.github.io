010e6912eea1065859d146545bce9ade52d89f01
Alexander Barmenkov <www.lad282@gmail.com>
2023-10-12 21:08:52 +0300

    added functional

---
+++ index.js
@@ -0,0 +1,14 @@
+const select = document.getElementById("list");
+const input = document.getElementById("count");
+const output = document.getElementById("answer");
+const errorlog = document.getElementById("errorlog");
+
+calculate = () => {
+    if (isNaN(input.value)) {
+        errorlog.style.display = "block";
+        errorlog.innerText = "Ошибка, введенные данные некорректны";
+        return -1;
+    }
+    errorlog.style.display = "none";
+    output.innerText = select.value * input.value;
+}
