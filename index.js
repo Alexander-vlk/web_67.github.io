10deb0ea12c2c5b01744c20e892ae9e149eefc5c
AlexBarmen <www.lad282@gmail.com>
2023-10-18 13:04:51 +0300

    added listener from js in index.js

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ calculate = () => {
     errorlog.style.display = "none";
     output.innerText = select.value * input.value;
 }
+
+const button = document.getElementById("button");
+window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    button.addEventListener("click", calculate);
+});
