a609963cd33164db1cf7b1cd06b9b4889c7e5d60
AlexBarmen <www.lad282@gmail.com>
2023-10-18 11:09:16 +0300

    added condition to calculate()

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const output = document.getElementById("answer");
 const errorlog = document.getElementById("errorlog");
 
 calculate = () => {
-    if (isNaN(input.value)) {
+    if (isNaN(input.value) || input.value < 0) {
         errorlog.style.display = "block";
         errorlog.innerText = "Ошибка, введенные данные некорректны";
         return -1;
